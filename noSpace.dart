a244162d554ecc32a5bf0e4b891c79e59af8b4a5
Wyatt Hoffman <wyatthoffman4392@gmail.com>
2021-08-06 08:08:52 -0700

    feat: update noSpace challenge to simplified solution with documentation

--- noSpace.dart
+++ noSpace.dart
@@ -1,6 +1,10 @@
 // Code Challenge: https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/dart
 // Description: Simple, remove the spaces from the string, then return the resultant string.
 
-String noSpace(String x) {
-  return x.replaceAll(RegExp(r' '), '');
-}
+// 1st Attempt
+// String noSpace(String x) {
+//   return x.replaceAll(RegExp(r' '), '');
+// }
+
+// Simplified version
+String noSpace(String x) => x.replaceAll(' ', '');
